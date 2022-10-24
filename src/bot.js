a7c7430ca27b54323fd6aeeaf8179424ccf767ef
Van-Huy <vanhuy.pham@student.vinci.be>
2022-10-24 17:17:21 +0200

    refactor: moving intentOptions into configs folder

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const { Client } = require('discord.js');
+const intentOptions = require('./configs/intentOptions.js');
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages', 'MessageContent'] });
+const client = new Client({ intents: intentOptions });
 const token = process.env.TOKEN;
 
 client.on('ready', () => {
 	console.log(`${client.user.tag} has logged in!`);
 });
-
 
 client.login(token);
