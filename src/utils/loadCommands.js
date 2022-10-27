131a30952c5ce44023d1cd5686b74d2a1e681e49
Van-Huy <vanhuy.pham@student.vinci.be>
2022-10-27 18:51:52 +0200

    feat: adding a file to load commands from its directory

---
+++ src/utils/loadCommands.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection } = require('discord.js');
+
+// Grab all the js command files from the commands directory
+const commandsPath = path.join(__dirname, '../commands');
+// Filter to ensure that only command js files are taken
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+let commandsCollection = new Collection();
+let commandsArray = [];
+
+const commandFilesCollection = () => {
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        // For each file being loaded, check that it has at least the data and execute properties
+        if ('data' in command && 'execute' in command) {
+             // Set a new item in the Collection with the key as the command name and the value as the exported module
+            commandsCollection.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+    }
+    return commandsCollection;
+}
+
+const commandFilesArray = () => {
+    for (const file of commandFiles) {
+        const command = require(`../commands/${file}`);
+        commandsArray.push(command.data.toJSON());
+    }
+    return commandsArray;
+}
+
+module.exports = { commandFilesCollection, commandFilesArray }
