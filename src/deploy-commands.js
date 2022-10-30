82d4b29f588a18bb36acedf0a32c09dacf404e5b
Van-Huy <vanhuy.pham@student.vinci.be>
2022-10-30 15:27:27 +0100

    feat: adding the code to deploy commands to the client

---
+++ src/deploy-commands.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const { REST, Routes } = require('discord.js');
+const { commandFilesArray } = require('./utils/loadCommands.js');
+
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+
+const commands = commandFilesArray();
+
+/***************************************************************************************
+* Author: Official discord.js Guide
+* Availability: https://discordjs.guide/creating-your-bot/command-deployment.html#guild-commands
+*
+***************************************************************************************/
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
+            Routes.applicationCommands(clientId, guildId),
+            { body: commands },
+        );
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        console.error(error);
+    }
+})();
