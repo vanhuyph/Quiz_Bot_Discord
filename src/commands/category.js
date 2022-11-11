bd562fafbab91274278618c0656ffa8afe29486f
Van-Huy <vanhuy.pham@student.vinci.be>
2022-11-11 21:11:44 +0100

    refactor: category command

--- src/commands/category.js
+++ src/commands/category.js
@@ -1,27 +1,23 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const axios = require('axios');
+const { categoriesCollection } = require('../utils/helperFunctions.js')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("category")
     .setDescription("Display all categories."),
   async execute(interaction) {
-    let data;
-    try {
-      data = await (await axios("https://opentdb.com/api_category.php")).data.trivia_categories;
-    } catch (error) {
-      console.log(error);
-      return await interaction.channel.send({ content: 'Something went wrong while trying to retrieve the categories... Please try again later!' });
-    }
-
-    const embedCategory = new EmbedBuilder().setTitle("All categories:").setTimestamp();
+    // Deferring the reply to allow the application fetching all the requested data
+    // otherwise the application will not respond in time
+    await interaction.deferReply();
+    const categories = await categoriesCollection();
+    const embedCategory = new EmbedBuilder().setTitle("📃 All categories 📃").setTimestamp();
 
     let msg = '';
-    for (let i = 0; i < data.length; i++) {
-      msg += '\n' + data[i].name;
+    for (let i = 0; i < categories.size; i++) {
+      msg += '\n▫️' + categories.at(i);
     }
     embedCategory.setDescription(msg);
 
-    await interaction.reply({ embeds: [embedCategory], fetchReply: true, ephemeral: true });
+    await interaction.editReply({ embeds: [embedCategory], fetchReply: true, ephemeral: true });
   }
 }
