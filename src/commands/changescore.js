d84bbe88308215ab003e2b0e9f566b1394081581
Van-Huy <vanhuy.pham@student.vinci.be>
2022-11-15 18:42:50 +0100

    refactor: changing to subcommand changeScore command

--- src/commands/changescore.js
+++ src/commands/changescore.js
@@ -3,28 +3,53 @@ const { addScore, displayScore } = require('../utils/helperFunctions.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('changescore')
-        .setDescription('Select a member and change his score points.')
-        .addUserOption(option =>
-            option.setName('target')
-                .setDescription('The member to change score points.')
-                .setRequired(true))
-        .addIntegerOption(option =>
-            option.setName('points')
-                .setDescription('Amount of points to add or remove.')
-                .setRequired(true))
+        .setName('change_score')
+        .setDescription('Select a member and show his points or change his score points.')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('user_info')
+                .setDescription('Show the score points of the user.')
+                .addUserOption(option =>
+                    option.setName('target')
+                        .setDescription('The user to show points.')
+                        .setRequired(true)
+                ))
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('user_points')
+                .setDescription('Change the score points of the user.')
+                .addUserOption(option =>
+                    option.setName('target')
+                        .setDescription('The member to change score.')
+                        .setRequired(true))
+                .addIntegerOption(option =>
+                    option.setName('points')
+                        .setDescription('The amount of points to add or remove.')
+                        .setRequired(true)
+                ))
         .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild),
     async execute(interaction) {
         // Retrieve the option values
         const user = interaction.options.getUser('target');
-        const points = interaction.options.getInteger('points');
 
-        await addScore(user, points);
-        const userScore = await displayScore(user.id);
+        if (interaction.options.getSubcommand() === 'user_info') {
+            const userScore = await displayScore(user.id);
 
-        return interaction.reply({
-            content: codeBlock(`Command successful. ${user.username} has now ${userScore} points.`),
-            ephemeral: true
-        });
+            return interaction.reply({
+                content: codeBlock(`${user.username} has ${userScore} points.`),
+                ephemeral: true
+            });
+        }
+        else if (interaction.options.getSubcommand() === 'user_points') {
+            const points = interaction.options.getInteger('points');
+
+            await addScore(user, points);
+            const userScore = await displayScore(user.id);
+
+            return interaction.reply({
+                content: codeBlock(`Change successful. ${user.username} has now ${userScore} points.`),
+                ephemeral: true
+            });
+        }
     }
 }
